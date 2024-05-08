17af0fe45b676a47e17c994c7c21e6470d06a644
rizingblare <rizingblare@gmail.com>
2024-05-09 01:35:41 +0900

    PS_240508

--- BaekJoon/Java/Main.java
+++ BaekJoon/Java/Main.java
@@ -2,6 +2,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*
+    1. 아이디어:
+
+    2. 시간 복잡도:
+
+    3. 자료구조:
+
+ */
+
 public class Main {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final StringBuilder sb = new StringBuilder();
