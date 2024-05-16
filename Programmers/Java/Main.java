888eac4204aa70681b0c460b862a3b33526d8d3a
Rizingblare <rizingblare@gmail.com>
2024-05-16 16:51:08 +0900

    [Rename] BaekJoon to BOJ

---
+++ Programmers/Java/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    /*
+        1. 아이디어:
+
+        2. 시간 복잡도:
+
+        3. 자료구조:
+
+     */
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+    }
+}
